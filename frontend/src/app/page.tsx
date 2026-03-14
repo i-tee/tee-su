c2d37d5941ae945de395d7f4f36fa4e9db476b20
itee <web@tee.su>
2026-03-14 15:43:20 +0300

    Initial commit: frontend + backend + docker

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Greeting from '@/components/Greeting'
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Привет, мир!</h1>
+      <Greeting />
+    </div>
+  )
+}
---
+++ frontend/src/components/Greeting.tsx
@@ -0,0 +1,8 @@
+export default function Greeting() {
+    return (
+        <div>
+            <h2>Я — компонент Greeting!</h2>
+            <p>Меня можно вставить в любую страницу</p>
+        </div>
+    )
+}
